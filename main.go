c5df416649397dfbb0066c2e956639089d45d6b9
“jubayer_ahmed” <jasamrat@gmail.com>
2023-02-10 17:34:47 +0600

    hi this is my second server

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 
+	//var x string
+
+	//var handler func(ResponseWriter, *Request)
+	http.HandleFunc("/", home)
+
+	http.ListenAndServe(":8888", nil)
+
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, `Hi! this is jubayer ahmed, welcome to my first golang webpage`)
 }
